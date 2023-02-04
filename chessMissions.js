241ec66838bfcc10c41cb7cad755b8ebf8e0e11d
Lobotomized <ivan_dzholev@abv.bg>
2023-02-04 18:46:33 +0200

    fix image size

--- chessMissions.js
+++ chessMissions.js
@@ -8,7 +8,7 @@ const { selectPiece, playerMove, checkTurn, changeTurn, lightBoard, closeLights,
 const {kingFactory, hatFactory, shroomFactory, northernKing, empoweredCrystalFactory} = require('./pieceDefinitions')
 app.use('/static', express.static('public'))
 app.use('/src', express.static('src'))
-
+app.use('/pieceDefinitions.js', express.static('pieces'))
 
 
 let lobby = newG({properties:{
